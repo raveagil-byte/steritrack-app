f581beb84342d65363a13650f7fe05925c91e685
raveagil-byte <raveagil@gmail.com>
2025-12-15 20:48:20 +0700

    Increase rate limit to 300 requests per 15 mins to fix 429 Too Many Requests dashboard

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use(bodyParser.json());
 // Rate Limiting
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
+    max: 300, // Limit each IP to 300 requests per windowMs
     message: 'Too many requests from this IP, please try again later.'
 });
 app.use('/api/', limiter); // Apply to all API routes
